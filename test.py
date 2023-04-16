8a6d94bcbb3a1daf75fdb8773ce2cb9e742c355f
IvanBespalov64 <ivan.a.bespalov@gmail.com>
2023-04-17 00:35:25 +0300

    Comments and README fixed

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ from wilson_b_matrix import get_current_derivative
 mol = Chem.MolFromMolFile('water.mol', removeHs=False)
 cart_derivatives = np.array([[9.8036462291277e-11, 3.3758505580049e-12, -1.1842328627332E-02],
 			     [-6.3571835660352e-11, 3.1612236176228e-04, 5.9211642889396e-03],
-			     [-3.4464626630925e-11, -3.1612236513812e-04, 5.9211643383920e-03]]).flatten() # Производные по декартовым координатам
+			     [-3.4464626630925e-11, -3.1612236513812e-04, 5.9211643383920e-03]]).flatten() # Производные по декартовым координатам (взяты из расчета в xtb)
 
 print(get_full_derivative(mol, cart_derivatives)) # Вернет список внутренних координат и список соответствующих им производных
 
 cur_coord = Angle(1, 0, 2)
-print(get_current_derivative(mol, cart_derivatives, cur_coord)) # Вернет производную по двугранному углу 0-1-2-3
+print(get_current_derivative(mol, cart_derivatives, cur_coord)) # Вернет производную по валентному углу H-O-H
